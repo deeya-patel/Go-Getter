d59195d298aeb250facebd11f3c7dfb833f3623d
deeya-patel <121900354+deeya-patel@users.noreply.github.com>
2023-10-08 10:57:33 -0400

    Create to-do-tracker.component.ts

---
+++ src/app/to-do-tracker/to-do-tracker.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-to-do-tracker',
+  templateUrl: './to-do-tracker.component.html',
+  styleUrls: ['./to-do-tracker.component.css']
+})
+export class ToDoTrackerComponent {
+}
+
